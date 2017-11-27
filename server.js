d02470a9d5159e310d76c227da736cf38a624a0a
takumon <inouetakumon@gmail.com>
2017-11-28 00:32:16 +0900

    publisher.htmlから送信したメッセージをsubscriber.htmlで受信できるようにした

--- server.js
+++ server.js
@@ -8,13 +8,38 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
 const server = express()
 
-// その他のリクエストはindexファイルにルーティング
+server.use((req, res, next) => {
+  res.sseSetup = () => res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+  res.sseSend = (data) => res.write("data: " + JSON.stringify(data) + "\n\n");
+  next()
+})
+
+var connections = []
+
+// メッセージ受信用のHandlerFunctionを作成
+const subscribeFunction = (req, res) => {
+  res.sseSetup()
+  connections.push(res)
+};
+
+// メッセージ送信用のHandlerFunctionを作成
+const publishFunction = (req, res) => {
+  if (!req.query.m) return res.sendStatus(400)
+  connections.forEach(con => con.sseSend(req.query.m))
+  res.sendStatus(200)
+};
+
+// ルーティングの定義
+server.get('/api/subscribe', subscribeFunction)
+server.get('/api/publish', publishFunction)
 server.get('/view/**', (req, res) => res.sendFile(path.join(__dirname, req.url)));
 server.get('/archive/**', (req, res) => res.sendFile(path.join(__dirname, req.url)));
-
 
 // サーバを起動
 server.listen(process.env.SERVER_PORT || 12345, () => {
@@ -23,4 +48,3 @@ server.listen(process.env.SERVER_PORT || 12345, () => {
   console.log('To stop the server, press Enter.')
   rl.on('line', () => process.exit());
 })
-
